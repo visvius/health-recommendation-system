6119208147eeb232fdc31cab97b7178a7deeaf38
visvius <vishal.singhofficial1@gmail.com>
2025-02-13 13:37:00 +0530

    moved large constants to constants.py

--- main.py
+++ main.py
@@ -1,13 +1,80 @@
-from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+import pickle
+from flask import Flask, request, render_template, jsonify
+from constants import diseases_list, symptoms_dict
+
+
 
 app = Flask(__name__)
 
-# routes
-@app.route('/')
-def index(): 
-    pass
-    # return render_template('index.html')
+# Load datasets
+dataset = pd.read_csv("datasets/Training.csv")
+sym_des = pd.read_csv("datasets/symtoms_df.csv")
+precautions = pd.read_csv("datasets/precautions_df.csv")
+workout = pd.read_csv("datasets/workout_df.csv")
+description = pd.read_csv("datasets/description.csv")
+medications = pd.read_csv('datasets/medications.csv')
+diets = pd.read_csv("datasets/diets.csv")
 
-# python main
-if __name__ == "__main__":
+# Load model
+svcLoad = pickle.load(open('models/svc.pkl','rb'))
+
+#============================================================
+# custom and helping functions
+#==========================helper funtions================
+def helper(dis):
+    desc = description[description['Disease'] == dis]['Description']
+    desc = " ".join([w for w in desc])
+
+    pre = precautions[precautions['Disease'] == dis][['Precaution_1', 'Precaution_2', 'Precaution_3', 'Precaution_4']]
+    pre = [col for col in pre.values]
+
+    med = medications[medications['Disease'] == dis]['Medication']
+    med = [med for med in med.values]
+
+    die = diets[diets['Disease'] == dis]['Diet']
+    die = [die for die in die.values]
+
+    wrkout = workout[workout['disease'] == dis] ['workout']
+
+    return desc,pre,med,die,wrkout
+
+
+
+# Model Prediction function
+def get_predicted_value(patient_symptoms):
+    input_vector = np.zeros(len(symptoms_dict))
+    for item in patient_symptoms:
+        input_vector[symptoms_dict[item]] = 1
+    return diseases_list[svcLoad.predict([input_vector])[0]]
+
+
+# Routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/developer')
+def developer():
+    return render_template("developer.html")
+
+@app.route('/blog')
+def blog():
+    return render_template("blog.html")
+
+
+# runs the program with auto-reload on updates
+if __name__ == '__main__':
     app.run(debug=True)
+
+
+# set-ExecutionPolicy RemoteSigned -Scope CurrentUser 
