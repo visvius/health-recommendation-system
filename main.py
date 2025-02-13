ede8b24267774b88002a2d246f24f495ffe1981a
visvius <vishal.singhofficial1@gmail.com>
2025-02-13 13:51:44 +0530

    sorted main.py function

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import pickle
 from flask import Flask, request, render_template, jsonify
 from constants import diseases_list, symptoms_dict
 
-
-
 app = Flask(__name__)
 
 # Load datasets
@@ -20,9 +18,14 @@ diets = pd.read_csv("datasets/diets.csv")
 # Load model
 svcLoad = pickle.load(open('models/svc.pkl','rb'))
 
-#============================================================
-# custom and helping functions
-#==========================helper funtions================
+# Model Prediction function
+def get_predicted_value(patient_symptoms):
+    input_vector = np.zeros(len(symptoms_dict))
+    for item in patient_symptoms:
+        input_vector[symptoms_dict[item]] = 1
+    return diseases_list[svcLoad.predict([input_vector])[0]]
+
+# Helper function
 def helper(dis):
     desc = description[description['Disease'] == dis]['Description']
     desc = " ".join([w for w in desc])
@@ -42,18 +45,37 @@ def helper(dis):
 
 
 
-# Model Prediction function
-def get_predicted_value(patient_symptoms):
-    input_vector = np.zeros(len(symptoms_dict))
-    for item in patient_symptoms:
-        input_vector[symptoms_dict[item]] = 1
-    return diseases_list[svcLoad.predict([input_vector])[0]]
-
-
 # Routes
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# Define a route for the home page
+@app.route('/predict', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        symptoms = request.form.get('symptoms')
+        print(symptoms)
+        if symptoms =="Symptoms":
+            message = "Please either write symptoms or you have written misspelled symptoms"
+            return render_template('index.html', message=message)
+        else:
+
+            # Split the user's input into a list of symptoms (assuming they are comma-separated)
+            user_symptoms = [s.strip() for s in symptoms.split(',')]
+            # Remove any extra characters, if any
+            user_symptoms = [symptom.strip("[]' ") for symptom in user_symptoms]
+            predicted_disease = get_predicted_value(user_symptoms)
+            dis_des, precautions, medications, rec_diet, workout = helper(predicted_disease)
+
+            my_precautions = []
+            for i in precautions[0]:
+                my_precautions.append(i)
+
+            return render_template('index.html', predicted_disease=predicted_disease, dis_des=dis_des, my_precautions=my_precautions, medications=medications, my_diet=rec_diet, workout=workout)
+
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
